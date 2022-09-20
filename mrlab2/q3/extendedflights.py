7e0c68ec0bdf1f74f6845e17789133df7b43488e
Caden S Summers <css85@e5-cse-trantor01.cse.psu.edu>
2022-09-20 18:55:53 -0400

    finished lab2

--- mrlab2/q3/extendedflights.py
+++ mrlab2/q3/extendedflights.py
@@ -6,11 +6,11 @@ class Flight(MRJob):  #MRJob version
         if flight[0] == "ITIN_ID":
             pass
         else:
-            num = float(flight[-1])
-            yield(flight[3], float(flight[-1]))
+            num = float(flight[-1]) # Convert number of passengers to float
+            yield(flight[3], float(flight[-1])) # Yield both state and airport the same number of passengers
             yield(flight[4], float(flight[-1]))
 
     def reducer(self, key, values):
-        yield(key, sum(values))
+        yield(key, sum(values)) # No change
 if __name__ == '__main__':
     Flight.run()   # MRJob version
